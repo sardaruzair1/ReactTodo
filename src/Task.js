24c4a91d77ba793f4bf4654e1ad7770e6ebd1665
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-02-03 01:42:15 +0500

    completed todos

--- src/Task.js
+++ src/Task.js
@@ -6,9 +6,9 @@ export const Task = (props)=>{
           <p>{props.taskName}</p>
           </div>
           <div className="btn-g">
-          
-          <button className='btn-success' onClick={()=> props.completeTask(props.id)}>/</button>
-          <button className='btn-danger' onClick={()=> props.deleteTask(props.id)}>X</button>
+          <button className='btn-success fa-solid fa-check' onClick={()=> props.completeTask(props.id)}></button>
+    
+          <button className='btn-danger fa-solid fa-xmark' onClick={()=> props.deleteTask(props.id)}></button>
           </div>
       
         </div>
